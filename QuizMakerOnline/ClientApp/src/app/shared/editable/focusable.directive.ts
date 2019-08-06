3f8d5c2e138e3617b79dcd2a044591cd21c29847
Miroslav Hlavicka <hlavicka@mf.cz>
2019-08-06 15:36:40 +0200

    login dialog css

--- QuizMakerOnline/ClientApp/src/app/shared/editable/focusable.directive.ts
+++ QuizMakerOnline/ClientApp/src/app/shared/editable/focusable.directive.ts
@@ -1,17 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[focusable]'
 })
 export class FocusableDirective {
 
+  @Input() focusDelay: number = 100;
+
   constructor(private host: ElementRef) { }
 
   ngAfterViewInit() {
-
-    setTimeout(() => { this.host.nativeElement.focus();  }, 100);
-
-    
+    setTimeout(() => { this.host.nativeElement.focus(); }, this.focusDelay);    
   }
-
 }
